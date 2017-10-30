6a2de18e932274488be37824ad81ed6daf6dc4ca
Александр <89229645123@ya.ru>
2017-10-30 20:11:30 +0300

    compited

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-    include './function.php';
-    $user = ['name' => 'Alex'];
-     echo render('main2', $user);
--- templace/main.php
+++ templace/main.php
@@ -1,9 +1,11 @@
 <html>
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,700&amp;subset=cyrillic" rel="stylesheet">
+<link rel="stylesheet" href="/public/styles/style.css" type="text/css" media="screen"/>
+
 <body>
-<h1>
-    sdsdsdsd
-    <?=$name?>
-</h1>
+    <header class="header">
+        <a href="/" class="braund">CONNECT</a>
+    </header>
 </body>
 
 </html>
