d1d82668865c6cd56ebe8a6ed3c28d0b59c3c391
stastotoro <32105780+stastotoro@users.noreply.github.com>
2017-11-08 19:58:12 +0300

    123

--- templates/index.php
+++ templates/index.php
@@ -1,1 +1,11 @@
-<h1>Главная</h1>
+<div> class="step.main"
+    <div> class="back"
+        <header class="header">
+            <a href="/" class="brand">CONNECT</a>
+            <nav class="header-links">
+                <a href="/contacts">Контакты</a>
+                <a href="/users">Пользователи</a>
+            </nav>
+        </header>
+    </div>
+</div>
