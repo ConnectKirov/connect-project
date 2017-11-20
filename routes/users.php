974b34d1177965894d2ef3bd694ac1efd58a51ca
Anton S <spaxi4@yandex.ru>
2017-11-20 21:12:34 +0300

    Функция fetch_users теперь принимает имя пользователя

--- routes/users.php
+++ routes/users.php
@@ -1,17 +1,14 @@
 <?php
 
 $router->get('/users', function () use ($app) {
-    $users = fetch_users(15);
+    $users = fetch_users();
     return $app->templating->renderWithLayout('users', [
         'users' => $users
     ]);
 });
 
 $router->get('/user', function ($params) use ($app) {
-    $users = fetch_users(15);
-    $users = array_filter($users, function (User $user) use ($params) {
-        return $user->firstName == $params['name'];
-    });
+    $users = fetch_users($params['name']);
 
     return $app->templating->renderWithLayout('users', [
         'users' => $users
